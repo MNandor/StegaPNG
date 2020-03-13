803340690128d00b231dabfe6f080ad2b401c119
MNandor <MNandor@users.noreply.github.com>
2020-03-14 00:03:54 +0200

    Added mixing by selecting pixels from each picture

--- stega.py
+++ stega.py
@@ -1,5 +1,6 @@
 import png
 import os
+import random
 
 #Note
 #For high contrast images, a detail level of 1 is recommended
@@ -14,7 +15,7 @@ def swap():
 	os.rename("msg.png", "org.png")
 	os.rename("tmp.png", "msg.png")
 
-
+#Incorrectly reverse for fun mixed pictures
 def fun():
 	os.mkdir("fun")
 	for i in range(1, 8):
@@ -90,3 +91,49 @@ def reverse(detail = 2):
 		with open("bac.png", "wb") as ofs:
 			writer = png.Writer(w,h, greyscale = False)
 			writer.write(ofs, resar)
+
+def mixPixel():
+	modes = [
+	lambda x, y: x % 2 == 0, #vertical strips
+	lambda x, y: y % 2 == 0, #horizontal strips
+	lambda x, y: (x+y) % 2 == 0, #checkerboard
+	lambda x, y: ((x+y)//4) % 2 == 0, #45 degree strips
+	lambda x, y: random.randint(0, 1) == 1, #random
+	lambda x, y: random.randint(0,y) > random.randint(0,x), #random crossfade
+	lambda x, y: x % 4 < y % 4 #triangles
+	]
+	
+	os.mkdir("mpi")
+	
+	for num, mode in enumerate(modes):
+	
+		with open("org.png", "rb") as ifsorg, open("msg.png", "rb") as ifsmsg:
+			org = png.Reader(file=ifsorg)
+			msg = png.Reader(file=ifsmsg)
+
+			orgar = org.read()
+			msgar = msg.read()
+			if orgar[0] != msgar[0] or orgar[1] != msgar[1]:
+				print("Wrong sizes")
+				exit()
+			
+			if orgar[3]["planes"] != msgar[3]["planes"]:
+				print("wrong channels")
+				exit()
+			
+			h = msgar[1]
+			w = msgar[0]
+			
+			orgar = list(orgar[2])
+			msgar = list(msgar[2])
+			
+			for y in range(h):
+				for x in range(w):
+					if mode(x, y):
+						for i in range(3*x, 3*x+3):
+							orgar[y][i] = msgar[y][i]
+						
+			
+			with open("mpi/res"+str(num)+".png", "wb") as ofs:
+				writer = png.Writer(w,h, greyscale = False)
+				writer.write(ofs, orgar)
